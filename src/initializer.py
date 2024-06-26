8a15bcdd56248ab598d648dbc35d3c3620e6972b
Idan Chen <idanwork22@gmail.com>
2024-06-26 16:26:29 +0300

    add basic folders structure

---
+++ src/initializer.py
@@ -0,0 +1,11 @@
+def get_app():
+    app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    # include all the routes
+    return app
